9d94075b21f400ecbb76850622845e1d8a3e348d
YuriBragaB <yuribragab1@gmail.com>
2026-05-30 13:03:56 -0300

    teste

--- aula.py
+++ aula.py
@@ -1,1 +1,3 @@
-#  kijreoi ururu
+# python-datenv, é uma biblioteca para carregar variáveis de ambiente a partir de um arquivo .env
+# é ótima para botar no .gitignore e não expor chaves de API, senhas, etc.
+
