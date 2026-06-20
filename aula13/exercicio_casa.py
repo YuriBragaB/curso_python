a55e8d2ca7ab735b8096e1711a1b8799f9e25ac1
YuriBragaB <yuribragab1@gmail.com>
2026-06-20 14:28:10 -0300

    intervalo

--- aula13/exercicio_casa.py
+++ aula13/exercicio_casa.py
@@ -111,127 +111,127 @@ supabase = create_client(supabase_url, supabase_key)
 
 
 def exibir_resposta(resposta : dict) -> None:
-    for dic in (resposta := resposta.data): # .data retorna um dic com listas dentro
-        for coluna, resp in dic.items():
-            print(f'{coluna} : {resp}')
-        print('-'* 50)
+	for dic in (resposta := resposta.data): # .data retorna um dic com listas dentro
+		for coluna, resp in dic.items():
+			print(f'{coluna} : {resp}')
+		print('-'* 50)
 
 # relacionamento entre as tabelas:
-    '''
-    2 - 1 = 1:1
-    3 - 2 = n:1
-    4 - 3 = n:n
-    5 - 4 = 1:n
-    '''
+	'''
+	2 - 1 = 1:1
+	3 - 2 = n:1
+	4 - 3 = n:n
+	5 - 4 = 1:n
+	'''
 
 # UPDATE:
 def atualizar_dados(tabela : str, id : int, dados : dict) -> None:
-    try:
-        resposta = supabase.table(tabela).update(dados).eq('id',id).execute()
-        print('Dados atualizados com sucesso')
-    except Exception as erro: # Exception é a classe pai de quase todos os erros padrão
-        print(f'Erro ao atualizar os dados: {erro}')  
+	try:
+		resposta = supabase.table(tabela).update(dados).eq('id',id).execute()
+		print('Dados atualizados com sucesso')
+	except Exception as erro: # Exception é a classe pai de quase todos os erros padrão
+		print(f'Erro ao atualizar os dados: {erro}')  
  
 def coletar_dados_e_atualizar():
-    print('Selecione a tabela que você deseja inserir:\n')
-    tabelas = {
-        1 : 'banco_cliente',
-        2 : 'banco_contas_bancarias',
-        3 : 'banco_transacoes',
-        4 : 'banco_servicos_bancarios',
-        5 : 'banco_contratacao_de_servicos'
-    }
-    campos_tabela = {
-        'banco_cliente' : ['id', 'nome', 'cpf', 'email', 'status' ],
-        'banco_contas_bancarias' : ['id', 'numero_da_conta', 'agencia', 'tipo_da_conta', 'data_de_abertura', 'saldo_inicial', 'cliente_id'],
-        'banco_transacoes' : ['id', 'tipo_da_transacao', 'data_da_trasacao', 'descricao', 'contas_bancarias_id'],
-        'banco_servicos_bancarios' : ['id', 'nome_do_servico', 'descricao', 'taxa_mensal', 'status', 'transacoes_id'],
-        'banco_contratacao_de_servicos' : ['id', 'status', 'observacao', 'data_da_contratacao', 'servicos_bancarios_id']
-    }
+	print('Selecione a tabela que você deseja inserir:\n')
+	tabelas = {
+		1 : 'banco_cliente',
+		2 : 'banco_contas_bancarias',
+		3 : 'banco_transacoes',
+		4 : 'banco_servicos_bancarios',
+		5 : 'banco_contratacao_de_servicos'
+	}
+	campos_tabela = {
+		'banco_cliente' : ['id', 'nome', 'cpf', 'email', 'status' ],
+		'banco_contas_bancarias' : ['id', 'numero_da_conta', 'agencia', 'tipo_da_conta', 'data_de_abertura', 'saldo_inicial', 'cliente_id'],
+		'banco_transacoes' : ['id', 'tipo_da_transacao', 'data_da_trasacao', 'descricao', 'contas_bancarias_id'],
+		'banco_servicos_bancarios' : ['id', 'nome_do_servico', 'descricao', 'taxa_mensal', 'status', 'transacoes_id'],
+		'banco_contratacao_de_servicos' : ['id', 'status', 'observacao', 'data_da_contratacao', 'servicos_bancarios_id']
+	}
 
-    for chave, valor in tabelas.items():
-        print(f'{chave} : {valor}')
+	for chave, valor in tabelas.items():
+		print(f'{chave} : {valor}')
 
-    tabela = int(input('\nDigite o número da tabela:\n'))
-    while tabela not in [1, 2, 3, 4, 5]:
-        os.system('cls')
-        print('Digite um número válido!\n')
+	tabela = int(input('\nDigite o número da tabela:\n'))
+	while tabela not in [1, 2, 3, 4, 5]:
+		os.system('cls')
+		print('Digite um número válido!\n')
 
-        for chave, valor in tabelas.items():
-            print(f'{chave} : {valor}')
+		for chave, valor in tabelas.items():
+			print(f'{chave} : {valor}')
 
-        tabela = int(input('\nDigite o número da tabela:\n'))
+		tabela = int(input('\nDigite o número da tabela:\n'))
 
-    tabela_selecionada = tabelas[tabela]
-    resposta = (supabase.table(tabela_selecionada)
-                .select('*')
-                .execute())
-    os.system('cls')
+	tabela_selecionada = tabelas[tabela]
+	resposta = (supabase.table(tabela_selecionada)
+				.select('*')
+				.execute())
+	os.system('cls')
 
-    print(f'\nA tabela selecionada foi: {tabela_selecionada}\n')
-    
-    print('-' * 50)
-    exibir_resposta(resposta)
+	print(f'\nA tabela selecionada foi: {tabela_selecionada}\n')
+	
+	print('-' * 50)
+	exibir_resposta(resposta)
 
-    id = int(input('\nDigite o ID que você deseja alterar:\n'))
+	id = int(input('\nDigite o ID que você deseja alterar:\n'))
 
-    resposta = supabase.table(tabela_selecionada).select('*').eq('id', id).execute()
-    aux = resposta.data[0] # pega o primeiro registro
-    novos_dados = {}
+	resposta = supabase.table(tabela_selecionada).select('*').eq('id', id).execute()
+	aux = resposta.data[0] # pega o primeiro registro
+	novos_dados = {}
 
-    for campo in campos_tabela[tabela_selecionada]:
-        print(f'{campo} : {aux[campo]}')
-        novos_dados[campo] = input(f'Novo valor:\n')
-        os.system('cls')
-    
-    atualizar_dados(tabela_selecionada, id, novos_dados)
+	for campo in campos_tabela[tabela_selecionada]:
+		print(f'{campo} : {aux[campo]}')
+		novos_dados[campo] = input(f'Novo valor:\n')
+		os.system('cls')
+	
+	atualizar_dados(tabela_selecionada, id, novos_dados)
 
 
 
 # DELETE
 def deletar_dados(tabela : str, id : int) -> None:
-    try:
-        resposta = supabase.table(tabela).delete().eq('id',id).execute()
-        print('Dados deletados')
-    except Exception as erro: # excepition é a classe pai de diversos erros em python
-        print(f'Erro ao deletar os dados: {erro}')
+	try:
+		resposta = supabase.table(tabela).delete().eq('id',id).execute()
+		print('Dados deletados')
+	except Exception as erro: # excepition é a classe pai de diversos erros em python
+		print(f'Erro ao deletar os dados: {erro}')
 
 def coletar_dados_e_deletar():
-    print('Selecione a tabela que você deseja deletar:\n')
-    tabelas = {
-        1 : 'banco_cliente',
-        2 : 'banco_contas_bancarias',
-        3 : 'banco_transacoes',
-        4 : 'banco_servicos_bancarios',
-        5 : 'banco_contratacao_de_servicos'
-    }
-    
-    for chave, valor in tabelas.items():
-        print(f'{chave} : {valor}')
+	print('Selecione a tabela que você deseja deletar:\n')
+	tabelas = {
+		1 : 'banco_cliente',
+		2 : 'banco_contas_bancarias',
+		3 : 'banco_transacoes',
+		4 : 'banco_servicos_bancarios',
+		5 : 'banco_contratacao_de_servicos'
+	}
+	
+	for chave, valor in tabelas.items():
+		print(f'{chave} : {valor}')
 
-    tabela = int(input('\nDigite o número da tabela:\n'))
-    while tabela not in [1, 2, 3, 4, 5]:
-        os.system('cls')
-        print('Digite um número válido!\n')
+	tabela = int(input('\nDigite o número da tabela:\n'))
+	while tabela not in [1, 2, 3, 4, 5]:
+		os.system('cls')
+		print('Digite um número válido!\n')
 
-        for chave, valor in tabelas.items():
-            print(f'{chave} : {valor}')
+		for chave, valor in tabelas.items():
+			print(f'{chave} : {valor}')
 
-        tabela = int(input('\nDigite o número da tabela:\n'))
+		tabela = int(input('\nDigite o número da tabela:\n'))
 
-    os.system('cls')
+	os.system('cls')
 
-    tabela_selecionada = tabelas[tabela]
-    resposta = (supabase
-                .table(tabela_selecionada)
-                .select('*')
-                .execute())
-    
-    print('-' * 50)
-    exibir_resposta(resposta)
+	tabela_selecionada = tabelas[tabela]
+	resposta = (supabase
+				.table(tabela_selecionada)
+				.select('*')
+				.execute())
+	
+	print('-' * 50)
+	exibir_resposta(resposta)
 
-    id = int(input('\nDigite o id da linha que você deseja deletar:\n'))
-    
-    deletar_dados(tabela_selecionada, id)
+	id = int(input('\nDigite o id da linha que você deseja deletar:\n'))
+	
+	deletar_dados(tabela_selecionada, id)
 
-coletar_dados_e_deletar()
+coletar_dados_e_atualizar()
--- aula15/aula15.py
+++ aula15/aula15.py
@@ -9,3 +9,33 @@ load_dotenv()
 supabase_url = os.getenv('SUPABASE_URL')
 supabase_key = os.getenv('SUPABASE_KEY')
 supabase = create_client(supabase_url, supabase_key)
+
+app = FastAPI()
+
+# Query strings, são strings de pesquisa
+
+@app.get("/busca")
+def busca(titulo: str = None, quantidade: int = None, genero: str = None, ano: int = None):
+
+    resposta = (supabase
+                .table('biblioteca_livro')
+                .select("*"))
+    
+    if titulo:
+        resposta = resposta.ilike("titulo", f'%{titulo}%')
+
+    if quantidade:
+        resposta = resposta.eq("quantidade", quantidade)
+
+    if genero:
+        resposta = resposta.ilike("genero", f"{genero}")
+        
+    if ano:
+        resposta = resposta.eq("ano", ano)
+
+    livros = resposta.execute()
+    if len(livros) == 0:
+        return {"Mensagem: Livro não encontrado"}
+    
+    return livros
+
