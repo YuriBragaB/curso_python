c0066f0eb4e7af7d7476fdcd4bc5003de3134dd7
YuriBragaB <yuribragab1@gmail.com>
2026-06-20 15:48:31 -0300

    final da aula

--- aula15/aula15.py
+++ aula15/aula15.py
@@ -1,7 +1,7 @@
 import os
 from supabase import create_client
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 import requests
 
 load_dotenv()
@@ -39,3 +39,106 @@ def busca(titulo: str = None, quantidade: int = None, genero: str = None, ano: i
     
     return livros
 
+# POST
+
+@app.post("/livros")
+def cadastrar_livro(dados: dict = Body()):
+    resposta = (
+                supabase
+                .table("biblioteca_livro")
+                .insert(dados)
+                .execute()
+                )
+    
+    resposta = resposta.data
+    return resposta
+
+@app.post("/autor")
+def cadastrar_autor(dados: dict = Body()):
+    resposta = (
+                supabase
+                .table("biblioteca_autor")
+                .insert(dados)
+                .execute()
+                )
+    
+    resposta = resposta.data
+    return resposta
+
+@app.post("/livro")
+def cadastrar_livro(dados: dict = Body()):
+    resposta = (
+                supabase
+                .table("biblioteca_usuario")
+                .insert(dados)
+                .execute()
+                )
+    
+    resposta = resposta.data
+    return resposta
+
+@app.post("/perfil")
+def cadastrar_perfil(dados: dict = Body()):
+    resposta = (
+                supabase
+                .table("biblioteca_perfil")
+                .insert(dados)
+                .execute()
+                )
+    
+    resposta = resposta.data
+    return resposta
+
+@app.post("/emprestimo")
+def cadastrar_emprestimo(dados: dict = Body()):
+    resposta = (
+                supabase
+                .table("biblioteca_livro")
+                .insert(dados)
+                .execute()
+                )
+    
+    resposta = resposta.data
+    return resposta
+
+def cadastrar(tabela: str, dados: dict = Body()):
+    resposta = (
+                supabase
+                .table(tabela)
+                .insert(dados)
+                .execute()
+                )
+    
+    resposta = resposta.data
+    return resposta
+
+# DELETE
+@app.delete("/deletarlivro/{id}")
+def deletar_livro(id: int = None):
+    resposta = (
+                supabase
+                .table("biblioeca_livro")
+                .delete()
+                .eq("id", id)
+                .execute
+                )
+    
+    return {
+        "msg": "livro deletado com sucesso",
+        }
+
+# UPDATE / put
+@app.put("/atualizarlivro/{id}")
+def atualizar_livro(id: int, dados:dict = Body()):
+    resposta = (
+        supabase
+        .table("biblioteca_livro")
+        .update(dados)
+        .eq('id', id)
+        .execute()
+    )
+
+    return {
+        "msg" : "livro atualizado com sucesso",
+        "resposta" : resposta.data
+    }
